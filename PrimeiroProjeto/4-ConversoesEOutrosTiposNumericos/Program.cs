9d7d6e495840307933bc3267932d19bd6d82033f
Rainer Ramos <rainermr89@hotmail.com>
2021-11-13 15:14:39 -0300

    Conversao e outros tipos numericos - parte 2

--- PrimeiroProjeto/4-ConversoesEOutrosTiposNumericos/Program.cs
+++ PrimeiroProjeto/4-ConversoesEOutrosTiposNumericos/Program.cs
@@ -10,6 +10,27 @@ namespace _4_ConversoesEOutrosTiposNumericos
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Executando o projeto 4");
+
+            double salario = 1200.50;
+
+            // O int é um tipo de variável que suporta valores até 32 bits
+            int salarioEmInteiro = (int)salario;
+
+            Console.WriteLine(salarioEmInteiro);
+
+            // O long é uma variável de 64 bits
+            long idade = 1300000000000;
+            Console.WriteLine(idade);
+
+            // O short é um tipo de variável de 16 bits
+            short quantidadeProdutos = 15000;
+            Console.WriteLine(quantidadeProdutos);
+
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+
+            Console.ReadLine();
         }
     }
 }
